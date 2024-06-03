f28e0cb35b1e8b7c86084bad1dc42dfb4fa5cb5c
Norbert49 <norboduor49@gmail.com>
2024-06-03 16:55:34 +0300

    additional changes to the source code

--- models/train_baseline_model.py
+++ models/train_baseline_model.py
@@ -12,15 +12,15 @@ sys.path.append(parent_dir)
 # Import from the preprocess submodule
 from data.preprocess import load_data, normalize_data
 
-# Import from the baseline_model submodule
+# Importing from the baseline_model submodule
 from models.baseline_model import create_baseline_model
 
 def main():
-    # Load and preprocess data
+    # Loading and preprocessing data
     x_train, y_train, x_test, y_test = load_data()
     x_train_normalized, x_test_normalized = normalize_data(x_train, x_test)
 
-    # Split the normalized training data into training and validation sets
+    # Splitting the normalized training data into training and validation sets
     x_train_normalized, x_val_normalized, y_train, y_val = train_test_split(x_train_normalized, y_train, test_size=0.2, random_state=42)
 
     print(f'Training set shape: {x_train_normalized.shape}, {y_train.shape}')
@@ -35,10 +35,10 @@ def main():
     y_val = tf.one_hot(y_val, depth=10)
     print(f'y_val shape after one-hot: {y_val.shape}')
 
-    # Define input shape
+    # Defining input shape
     input_shape = x_train_normalized[0].shape
 
-    # Train the model
+    # Training the model
     baseline_model = create_baseline_model(input_shape=input_shape)
     history = baseline_model.fit(x_train_normalized, y_train, epochs=10, batch_size=32, validation_data=(x_val_normalized, y_val))
 
